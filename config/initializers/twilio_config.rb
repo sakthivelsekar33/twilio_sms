3d1914ba4b59f1a6d47be5a4787354dad4f3da95
Sakthivel S <sakthivelsekar33@gmail.com>
2013-07-12 16:39:28 +0530

    Second Commit

--- config/initializers/twilio_config.rb
+++ config/initializers/twilio_config.rb
@@ -1,3 +1,3 @@
-CarterRails::Application.config.account_sid = "ACe95c7ceca45a07e3518360a2458c1393"
-CarterRails::Application.config.auth_token = "1ef65e27e8aec2e0cfbb4aadba790477"
-CarterRails::Application.config.phone_number = "(469) 267-6645"
+CarterRails::Application.config.account_sid = "AC66e6d5db30c0f26ad9d7c967964b9308"
+CarterRails::Application.config.auth_token = "a1eb71af4a01e5afbb8aeee2c1be8f68"
+CarterRails::Application.config.phone_number = "469-267-6645"
