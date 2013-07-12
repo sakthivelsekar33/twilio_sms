e8ab7e5b40836324b6145bdcc444c04371c6d8da
Sakthivel S <sakthivelsekar33@gmail.com>
2013-07-12 17:10:04 +0530

    Third Commit

--- config/initializers/twilio_config.rb
+++ config/initializers/twilio_config.rb
@@ -1,3 +1,3 @@
 CarterRails::Application.config.account_sid = "AC66e6d5db30c0f26ad9d7c967964b9308"
 CarterRails::Application.config.auth_token = "a1eb71af4a01e5afbb8aeee2c1be8f68"
-CarterRails::Application.config.phone_number = "469-267-6645"
+CarterRails::Application.config.phone_number = "917736407859"
