593690a914c9b9316bcd83fbd18dd731bd3ddf73
Sakthivel S <sakthivelsekar33@gmail.com>
2013-07-12 14:50:29 +0530

    Initial Commit

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,7 +1,7 @@
 class TwilioController < ApplicationController
 
   def process_sms
-    @city = params[:FromCity].capitalize
+    @city = params[:FromCity]#.capitalize
     @state = params[:FromState]
     render 'process_sms.xml.erb', :content_type => 'text/xml'
   end
--- config/initializers/twilio_config.rb
+++ config/initializers/twilio_config.rb
@@ -1,3 +1,3 @@
-CarterRails::Application.config.account_sid = "YOUR SID HERE"
-CarterRails::Application.config.auth_token = "YOUR AUTH TOKEN HERE"
-CarterRails::Application.config.phone_number = "YOUR PHONE NUMBER HERE"
+CarterRails::Application.config.account_sid = "ACe95c7ceca45a07e3518360a2458c1393"
+CarterRails::Application.config.auth_token = "1ef65e27e8aec2e0cfbb4aadba790477"
+CarterRails::Application.config.phone_number = "(469) 267-6645"
